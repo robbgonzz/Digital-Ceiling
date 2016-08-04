af2dd1e3dd556b96c2fc6ad4ab8bca65a3e6d0f1
robbgonzz <robgonz03@yahoo.com>
2016-08-04 16:08:52 -0700

    first commit

---
+++ Documents/DigitalCeilings/Digital Ceilings/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Digital Ceilings
+//
+//  Created by Robert Gonzalez on 7/20/16.
+//  Copyright © 2016 Robert Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func Loggin(sender: AnyObject) {
+        
+    }
+
+}
+
